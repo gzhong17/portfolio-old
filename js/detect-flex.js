c043105d9c06b2527fbfd4558461d0d162684f3c
Gloria Zhong <gzhong17@hotmail.com>
2017-01-14 17:42:33 -0800

    Restructuring description text of works and other small adjustments

--- js/detect-flex.js
+++ js/detect-flex.js
@@ -1,9 +1,12 @@
 $(function(){
 	var grids = $(".works");
+	var cols = $(".columns");
 
 	if (Modernizr.flexbox) {
-		grids.addClass("flex");		
+		grids.addClass("flex");
+		cols.addClass("flex");		
 	} else{
 		grids.removeClass("flex");
+		cols.removeClass("flex");
 	}
 });
